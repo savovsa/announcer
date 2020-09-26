b19790f36c74caca93f0bff1cfb64c5d56278081
Sasho Savov <savovs@gmail.com>
2020-09-26 19:02:27 +0300

    Serialize Messages
    Co-authored-by: Vlady Veselinov <vladkodmc@gmail.com>

--- src/messages.rs
+++ src/messages.rs
@@ -1,11 +1,20 @@
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+#[derive(Serialize, Deserialize)]
+pub struct Config<'a> {
+    pub audio_folder_path: &'a str,
+    pub messages: Messages<'a>,
+}
+
+type Messages<'a> = HashMap<&'a str, Message<'a>>;
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct Config {
-    pub message: String,
+pub struct Message<'a> {
+    pub name: &'a str,
+    pub volume: f32,
 }
 
 pub fn save_config(config: Config) {
     let file = std::fs::File::create("config.json").unwrap();
-    serde_json::to_writer_pretty(file, &config);
+    serde_json::to_writer_pretty(file, &config).unwrap();
 }
