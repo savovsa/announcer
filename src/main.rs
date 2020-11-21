21e4df68f9f5266ea627c1cc79e3bf2f6476db4b
Sasho Savov <savovs@gmail.com>
2020-11-21 17:30:28 +0200

    Adding event listener for config.json
    Co-authored-by: Vlady Veselinov <vladkodmc@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,31 @@
 use announcer::messages::{load_config, save_config, Config, Message};
+use notify::{
+    event::ModifyKind, Error, Event, EventFn, EventKind, RecommendedWatcher, RecursiveMode, Watcher,
+};
 use tide::{http, Body, Response};
+
+const CONFIG_PATH: &str = "config.json";
 
 #[async_std::main]
 async fn main() -> tide::Result<()> {
-    let config = load_config("config.json").unwrap();
+    let mut config = load_config(CONFIG_PATH).unwrap();
+
+    let mut watcher: RecommendedWatcher =
+        Watcher::new_immediate(|result: Result<Event, Error>| {
+            let event = result.unwrap();
+            if event.kind == EventKind::Modify(ModifyKind::Any) {
+                // reload config
+            }
+        })?;
+
+    watcher.watch(CONFIG_PATH, RecursiveMode::Recursive)?;
 
     let mut app = tide::with_state(config);
 
     app.at("/messages").get(get_messages);
     app.at("/message/:name").get(get_message);
     app.listen("127.0.0.1:8080").await?;
+
     Ok(())
 }
 
