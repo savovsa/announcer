ef15589598f09a46ca9d6039f05ee36d8d15b423
Sasho Savov <savovs@gmail.com>
2021-03-21 00:29:48 +0200

    begin of testing upload message process
    Co-authored-by: Vlady Veselinov <vladinator1000@users.noreply.github.com>

--- tests/file_upload.rs
+++ tests/file_upload.rs
@@ -1,7 +1,10 @@
-use announcer::messages::load_config;
+use announcer::messages::{load_config, Message};
 use announcer::{create_app, upload};
 use async_std;
+use serde::{Deserialize, Serialize};
 use surf::{self, Body};
+
+const MP3_FILE_PATH: &str = "tests/soft-bells.mp3";
 
 #[async_std::test]
 async fn audio_file_gets_saved() -> surf::Result<()> {
@@ -11,7 +14,7 @@ async fn audio_file_gets_saved() -> surf::Result<()> {
 
     let app = create_app().unwrap();
 
-    let body = Body::from_file("tests/soft-bells.mp3").await?;
+    let body = Body::from_file(MP3_FILE_PATH).await?;
     let res = surf::Client::with_http_client(app)
         .put(uri)
         .body(body)
@@ -58,4 +61,46 @@ async fn non_audio_file_doesnt_get_saved() {
     let body = res.body_string().await.unwrap();
     assert_eq!(body, upload::UNRECOGNIZED_FILE_FORMAT_ERROR_MESSAGE);
     assert!(!file_exists);
+}
+
+#[derive(Serialize, Deserialize)]
+struct FileWithMeta {
+    file: Vec<u8>,
+    meta: Message,
+}
+
+#[async_std::test]
+async fn config_is_updated_after_successful_uploading() {
+    let file_name = "soft-bells.mp3";
+    let file_path = std::path::Path::new("sounds").join(file_name);
+    let uri = "http://localhost:8080/upload/soft-bells.mp3";
+
+    let app = create_app().unwrap();
+
+    let file = std::fs::read(MP3_FILE_PATH).unwrap();
+
+    let file_with_meta = FileWithMeta {
+        file,
+        meta: Message {
+            volume: 1.0,
+            display_name: "soft-bells.mp3".to_string(),
+        },
+    };
+    let body = surf::Body::from_json(&file_with_meta).unwrap();
+
+    surf::Client::with_http_client(app)
+        .put(uri)
+        .body(body)
+        .await
+        .unwrap();
+
+    let file_exists = std::fs::metadata(file_path.clone()).is_ok();
+
+    if file_exists {
+        std::fs::remove_file(file_path).unwrap();
+    }
+
+    // TODO:
+    // load configuration stored in json file
+    // check message
 }
