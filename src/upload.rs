8d283f0faca6b8a8bb6ae261d2be3925f14e6079
Sasho Savov <savovs@gmail.com>
2021-03-20 16:11:59 +0200

    File verification (#2)
    * test uploading
    
    * Parsing audio
    
    Co-authored-by: Vlady Veselinov <vladinator1000@users.noreply.github.com>
    
    * fix borow errors
    
    Co-authored-by: Vlady Veselinov <vladinator1000@users.noreply.github.com>
    
    * Error handling
    
    * Cleanup
    
    Co-authored-by: Vlady Veselinov <vladinator1000@users.noreply.github.com>
    
    * New line added
    
    Co-authored-by: Vlady Veselinov <vladinator1000@users.noreply.github.com>

--- src/upload.rs
+++ src/upload.rs
@@ -1,10 +1,29 @@
+use crate::Request;
+use rodio;
+
+pub const UNRECOGNIZED_FILE_FORMAT_ERROR_MESSAGE: &str =
+    "Unrecognized file format, please upload MP3, WAV, Vorbis or FLAC.";
+
 pub mod endpoints {
-    use crate::{messages::Config, Request};
+    use super::*;
+    use crate::Request;
     use async_std::{fs::OpenOptions, io};
     use std::path::Path;
     use tide;
 
     pub async fn upload(mut req: Request) -> tide::Result {
+        // Using a &mut because reading the request body as bytes requires it
+        let parsing_result = parse_audio_file(&mut req).await;
+
+        if parsing_result.is_err() {
+            let mut res = tide::Response::new(400);
+            res.set_body(UNRECOGNIZED_FILE_FORMAT_ERROR_MESSAGE);
+
+            return Ok(res);
+        }
+
+        let bytes = parsing_result.unwrap();
+
         let name: String = req.param("name")?.parse()?;
 
         let file_path = {
@@ -18,11 +37,17 @@ pub mod endpoints {
             .open(&file_path)
             .await?;
 
-        let body = req.body_bytes().await.unwrap();
-
-        io::copy(io::Cursor::new(body), file).await?;
+        io::copy(io::Cursor::new(bytes), file).await?;
 
         let res = tide::Response::new(200);
         Ok(res)
     }
+}
+
+async fn parse_audio_file(req: &mut Request) -> Result<Vec<u8>, rodio::decoder::DecoderError> {
+    let bytes = req.body_bytes().await.unwrap();
+    let cursor = std::io::Cursor::new(bytes.clone());
+    let rodio_result = rodio::decoder::Decoder::new(cursor);
+
+    rodio_result.map(|_| bytes)
 }
--- tests/file_upload.rs
+++ tests/file_upload.rs
@@ -1,19 +1,20 @@
-use announcer::create_app;
 use announcer::messages::load_config;
+use announcer::{create_app, upload};
 use async_std;
-use surf;
+use surf::{self, Body};
 
 #[async_std::test]
-async fn file_gets_saved() -> surf::Result<()> {
-    let file_name = "hello.wav";
+async fn audio_file_gets_saved() -> surf::Result<()> {
+    let file_name = "soft-bells.mp3";
     let file_path = std::path::Path::new("sounds").join(file_name);
-    let uri = "http://localhost:8080/upload/hello.wav";
-    let data = load_config("tests/messages_test_config.json").unwrap();
+    let uri = "http://localhost:8080/upload/soft-bells.mp3";
 
     let app = create_app().unwrap();
+
+    let body = Body::from_file("tests/soft-bells.mp3").await?;
     let res = surf::Client::with_http_client(app)
         .put(uri)
-        .body(surf::Body::from_json(&data)?)
+        .body(body)
         .await?;
 
     let file_exists = std::fs::metadata(file_path.clone()).is_ok();
@@ -22,8 +23,39 @@ async fn file_gets_saved() -> surf::Result<()> {
         std::fs::remove_file(file_path).unwrap();
     }
 
+    // TODO: Find a nicer way to test things like this
+    // that prints why an error happened
     assert_eq!(res.status(), surf::StatusCode::Ok);
     assert!(file_exists);
 
     Ok(())
+}
+
+#[async_std::test]
+async fn non_audio_file_doesnt_get_saved() {
+    let file_name = "hello.wav";
+    let file_path = std::path::Path::new("sounds").join(file_name);
+    let uri = "http://localhost:8080/upload/hello.wav";
+    let data = load_config("tests/messages_test_config.json").unwrap();
+
+    let app = create_app().unwrap();
+    let mut res = surf::Client::with_http_client(app)
+        .put(uri)
+        .body(surf::Body::from_json(&data).unwrap())
+        .await
+        .unwrap();
+
+    let file_exists = std::fs::metadata(file_path.clone()).is_ok();
+
+    if file_exists {
+        std::fs::remove_file(file_path).unwrap();
+    }
+
+    // TODO: Find a nicer way to test things like this
+    // that prints why an error happened
+    assert_eq!(res.status(), surf::StatusCode::BadRequest);
+
+    let body = res.body_string().await.unwrap();
+    assert_eq!(body, upload::UNRECOGNIZED_FILE_FORMAT_ERROR_MESSAGE);
+    assert!(!file_exists);
 }
